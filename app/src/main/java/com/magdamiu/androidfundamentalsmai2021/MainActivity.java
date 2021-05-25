46e553adbe14f37f50ff6f969da434ce5d1e5e76
Tomescu-Alexandru <alexandrutomescu23@gmail.com>
2021-05-25 18:32:02 +0300

    added interface

--- app/src/main/java/com/magdamiu/androidfundamentalsmai2021/MainActivity.java
+++ app/src/main/java/com/magdamiu/androidfundamentalsmai2021/MainActivity.java
@@ -20,8 +20,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         /*setContentView(R.layout.activity_main);*/
-        setContentView(R.layout.sample);
-
+//        setContentView(R.layout.sample);
+        setContentView(R.layout.code_challenge);
         initViews();
         displayLogs();
     }
